15e565a641db423dc1dc411591eb2475a7bed7eb
dougneedham <sheridan300@yahoo.com>
2014-10-19 19:37:58 +0000

    Adding the fake Map-Reduce code

---
+++ mapper.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import sys
+# awk -F '|' '{print $2;'} /HM/HomeMiner_Recorder_001.txt  | ./mapper.py  | sort | ./counter.py | sort -k2 -n
+# input comes from STDIN (standard input)
+for line in sys.stdin:
+    # remove leading and trailing whitespace
+    line = line.strip()
+    # split the line into words
+    words = line.split()
+    # increase counters
+    for word in words:
+        # write the results to STDOUT (standard output);
+        # what we output here will be the input for the
+        # Reduce step, i.e. the input for reducer.py
+        #
+        # tab-delimited; the trivial word count is 1
+        print '%s\t%s' % (word, 1)
+
